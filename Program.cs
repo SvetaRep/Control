76e6762d07735591c7e00a4b63840b89b9ef65d2
Svetlana <svetarepalenko@yandex.ru>
2022-10-02 22:35:51 +0300

    add programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = Array();
+string[] result = FindArray(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+string[] FindArray(string[] input, int n) 
+{
+    string[] output =new string[GetFindArray(input, n)];
+    for(int i=0, j=0; i<input.Length; i++) 
+    {
+        if(input[i].Length<=n) 
+        {
+            output[j]=input[i];
+            j++;
+        }
+    }
+    return output;
+}
+int GetFindArray(string[] input, int n) 
+{
+    int count=0;
+    for(int i=0; i<input.Length; i++) 
+    {
+        if(input[i].Length<=n) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
+string[] Array() 
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
